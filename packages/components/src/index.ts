cb3f05ca513ed0e97f142030d2e55e9b5e9b9e91
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2026-01-14 19:10:05 +0900

    refactor: modal 경로 삭제

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,6 +1,5 @@
 import '@dds-app/foundation';
 
 export { Switch, FormButton } from './buttons';
-export { Modal } from './modals/Modal';
 export { DatePickerModal } from './modals/DatePicker';
 export { TimePickerModal } from './modals/TimePicker';
