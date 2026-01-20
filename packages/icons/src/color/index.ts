423e57ceaba86dc829b2935e34c3b11bbc29970a
kmg <legolove08@naver.com>
2026-01-20 22:49:16 +0900

    feat: 아이콘 컴포넌트 구현 및 IconPage 추가

---
+++ packages/icons/src/color/index.ts
@@ -0,0 +1,48 @@
+import { BarChart } from './BarChart';
+import { Bullseye } from './Bullseye';
+import { ConvenienceStore } from './ConvenienceStore';
+import { CookedRice } from './CookedRice';
+import { CreditCard } from './CreditCard';
+import { Dgit } from './Dgit';
+import { FullMoonFace } from './FullMoonFace';
+import { Handshake } from './Handshake';
+import { LoudSpeaker } from './LoudSpeaker';
+import { MusicalNote } from './MusicalNote';
+import { SchoolBus } from './SchoolBus';
+import { SmilingFace } from './SmilingFace';
+import { Tent } from './Tent';
+import { Trophy } from './Trophy';
+
+export {
+  BarChart,
+  Bullseye,
+  ConvenienceStore,
+  CookedRice,
+  CreditCard,
+  Dgit,
+  FullMoonFace,
+  Handshake,
+  LoudSpeaker,
+  MusicalNote,
+  SchoolBus,
+  SmilingFace,
+  Tent,
+  Trophy,
+};
+
+export const ColorIcon = {
+  BarChart,
+  Bullseye,
+  ConvenienceStore,
+  CookedRice,
+  CreditCard,
+  Dgit,
+  FullMoonFace,
+  Handshake,
+  LoudSpeaker,
+  MusicalNote,
+  SchoolBus,
+  SmilingFace,
+  Tent,
+  Trophy,
+};
